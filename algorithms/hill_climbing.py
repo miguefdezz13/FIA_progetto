9ab2cea7e695b9ef5bbac65448f5138220ce29c0
miguel123z88 <miguelcrtess17@gmail.com>
2026-02-10 15:38:41 +0100

    Update hill_climbing.py

--- algorithms/hill_climbing.py
+++ algorithms/hill_climbing.py
@@ -16,3 +16,31 @@ We will attempt to generate a full path or a sequence of moves by iterating.
 If we get stuck (local optimum), we accept a bad move with probability P.
 """
 
+
+
+import random
+import math
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from environment.cell import TerrainType
+
+def manhattan_distance(a, b):
+    return abs(a.r - b.r) * abs(a.c - b.c)
+
+def hill_climbing_scent(grid, start_cell, ants, max_iterations=1000):
+    """
+    Hill Climbing based on SCENT (Pheromone).
+    The Anteater climbs the gradient of HIGHEST Pheromone level.
+    """
+    grid.update_scent([])
+
+    current = start_cell
+    path = [current]
+    explored_nodes = path
+
+    T_initial = 100.0
+    alpha = 1.05
+    T = T_initial
+
